0e0a5b09410f3a0e2eab8fbd5575558692445b63
Anthony A. Tirabassi (T) <143787394+t-tirabassi@users.noreply.github.com>
2024-09-05 13:22:30 -1000

    dashboard tab added

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -35,8 +35,9 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
           <Nav className="me-auto justify-content-end basic-nav">
-            <NavLink to="/home"><Image style={{ marginTop: '8px', marginBottom: '20px', marginRight: '15px', width: '200px', height: shrink ? '75px' : '95px' }} src="/images/spirebooks-logo.png" /></NavLink>
+            <NavLink to="/home"><Image style={{ marginTop: '8px', marginBottom: '20px', marginRight: '10px', width: '200px', height: shrink ? '75px' : '95px' }} src="/images/spirebooks-logo.png" /></NavLink>
             {currentUser ? ([
+              <Nav.Link className="nav-tabs" id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">DASHBOARD</Nav.Link>,
               <Nav.Link className="nav-tabs" id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} to="/add" key="add">FINANCES</Nav.Link>,
               <Nav.Link className="nav-tabs" id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">AUDIT</Nav.Link>,
               <Nav.Link className="nav-tabs" id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list">VISUALIZE</Nav.Link>,

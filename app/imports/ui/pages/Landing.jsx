eebfa7c17478010a531edb13b36abe85d3f34a16
Samantha Mallari <samiam2204@gmail.com>
2024-09-09 10:44:48 -1000

    Added image

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Container, Image, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-/* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        {
-          // <a href="/tos" className="btn btn-secondary" role="button" id="button">idk where to put this, but here&#39s the tos!!!!!!!!!</a>
-        }
-      </Col>
-
+    <Row>
+      <Image className="mx-auto" src="/images/landing.jpg" />
     </Row>
+    <Row className="text-center">
+      <h1>SpireBooks</h1>
+    </Row>
+    <hr className="gradient" />
   </Container>
 );
 

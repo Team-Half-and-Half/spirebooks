efc3dfa83b6acc91bdfaac21bcb765304f68b25d
PaytonHAH <paytonh@hawaii.edu>
2024-09-10 22:29:47 -1000

    Added more fields

--- app/imports/ui/pages/AddMoney.jsx
+++ app/imports/ui/pages/AddMoney.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, NumField, SubmitField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
@@ -128,7 +128,43 @@ const formSchema = new SimpleSchema({
     defaultValue: 0,
     min: 0,
   },
-  // Capital Assets
+  // Capital Assets (Assets)
+  buildings: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  leaseholdImprovements: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  furnitureFixturesAndEquipment: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  lessAccumulatedDepreciation: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  // Capital Assets (Land)
+  landA: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  landB: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
+  constructionInProgress: {
+    type: Number,
+    defaultValue: 0,
+    min: 0,
+  },
   condition: {
     type: String,
     allowedValues: ['dawg', 'good', 'fair', 'poor'],
@@ -158,7 +194,7 @@ const AddMoney = () => {
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
   return (
-    <Container id={PAGE_IDS.ADD_STUFF} className="py-3">
+    <Container fluid id={PAGE_IDS.ADD_STUFF} className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
           <Col className="text-center"><h2>Custom Balance Sheet</h2></Col>
@@ -179,7 +215,7 @@ const AddMoney = () => {
                 <NumField name="notesReceivableDueAfterOneYear" decimal={null} />
                 <NumField name="securityDeposits" decimal={null} />
                 <NumField name="cashHeldByInvestmentManager" decimal={null} />
-                <h6>INVESTMENTS</h6>
+                <h4>INVESTMENTS</h4>
                 <NumField name="mutualFunds" decimal={null} />
                 <NumField name="commingledFunds" decimal={null} />
                 <NumField name="hedgeFunds" decimal={null} />
@@ -188,10 +224,20 @@ const AddMoney = () => {
                 <NumField name="commonAndPreferredStock" decimal={null} />
                 <NumField name="privateDebt" decimal={null} />
                 <NumField name="other" decimal={null} />
-                <h6>U.S INVESTMENTS</h6>
+                <h4>U.S INVESTMENTS</h4>
                 <NumField name="treasuries" decimal={null} />
                 <NumField name="agencies" decimal={null} />
                 <NumField name="other" decimal={null} />
+                <h4>Capital Assets, net:</h4>
+                <h6>Assets</h6>
+                <NumField name="buildings" decimal={null} />
+                <NumField name="leaseholdImprovements" decimal={null} />
+                <NumField name="furnitureFixturesAndEquipment" decimal={null} />
+                <NumField name="lessAccumulatedDepreciation" decimal={null} />
+                <h6>Land</h6>
+                <NumField name="landA" decimal={null} />
+                <NumField name="landB" decimal={null} />
+                <NumField name="constructionInProgress" decimal={null} />
                 <SubmitField value="Submit" />
                 <ErrorsField />
               </Card.Body>

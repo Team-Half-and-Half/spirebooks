ccb28417326f74a37e8b7d9239fd423948f82733
PaytonHAH <paytonh@hawaii.edu>
2024-09-18 10:03:52 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -9,8 +9,8 @@ import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const credentials = { username: 'accountant@spirebooks.com', password: 'spirebooksadmin' };
-const adminCredentials = { username: 'customer@spirebooks.com', password: 'spirebookscustomer' };
+const adminCredentials = { username: 'accountant@spirebooks.com', password: 'spirebooksadmin' };
+const credentials = { username: 'customer@spirebooks.com', password: 'spirebookscustomer' };
 const newCredentials = { username: 'newcustomer@spirebooks.com', password: 'spirebooksnewcustomer' };
 
 fixture('meteor-application-template-production localhost test with default db')
@@ -26,20 +26,10 @@ test('Test that signin and signout work', async () => {
   await navBar.isLoggedIn(credentials.username);
   await navBar.logout();
   await signOutPage.isDisplayed();
-});
-
-test('Test that user pages show up', async () => {
+  // Admin Login/Logout
   await navBar.gotoSignInPage();
-  await signInPage.signin(credentials.username, credentials.password);
-  await navBar.isLoggedIn(credentials.username);
-  await navBar.gotoAddStuffPage();
-  await addStuffPage.isDisplayed();
-  await navBar.gotoListStuffPage();
-  await listStuffPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  await t.click(editLinks.nth(0));
-  await editStuffPage.isDisplayed();
+  await signInPage.signin(adminCredentials.username, adminCredentials.password);
+  await navBar.isLoggedIn(adminCredentials.username);
   await navBar.logout();
   await signOutPage.isDisplayed();
 });
@@ -51,22 +41,4 @@ test('Test that sign up and sign out work', async () => {
   await navBar.isLoggedIn(newCredentials.username);
   await navBar.logout();
   await signOutPage.isDisplayed();
-});
-
-test('Test that admin pages show up', async () => {
-  await navBar.gotoSignInPage();
-  await signInPage.signin(adminCredentials.username, adminCredentials.password);
-  await navBar.isLoggedIn(adminCredentials.username);
-  await navBar.gotoAddStuffPage();
-  await addStuffPage.isDisplayed();
-  await navBar.gotoListStuffPage();
-  await listStuffPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  await t.click(editLinks.nth(0));
-  await editStuffPage.isDisplayed();
-  await navBar.gotoListStuffAdminPage();
-  await listStuffAdminPage.isDisplayed();
-  // await navBar.gotoManageDatabasePage();
-  // await manageDatabasePage.isDisplayed();
 });

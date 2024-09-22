49365c55cbb72bb31745ae2df5a3e89ae0739507
Nicholas Beydler <beydlern@hawaii.edu>
2024-09-21 21:10:51 -1000

    Verification boolean value added
    Every newly created user will have an associated verification boolean value (set to false by default) that will be assigned to them upon account creation. The verification boolean value sits in the UserVerificationCollection collection where the primary key is the userID of the user in the UserProfileCollection collection.

---
+++ app/imports/api/user/UserVerificationCollection.js
@@ -0,0 +1,28 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+/**
+ * The UserVerificationCollection.
+ *
+ */
+class UserVerificationCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'UserVerificationCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      verification: {
+        type: Boolean,
+        defaultValue: false,
+      },
+      userID: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+export const UserVerification = new UserVerificationCollection();

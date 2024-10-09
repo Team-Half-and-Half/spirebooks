5335782f2185a46c97a3fd6c61d3b8e19df5e7eb
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2024-10-09 09:07:23 -1000

    Made it so line names can be changed based off the name of the data

--- app/imports/ui/components/CustomLineChart.jsx
+++ app/imports/ui/components/CustomLineChart.jsx
@@ -2,26 +2,38 @@ import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, X
 import React from 'react';
 import PropTypes from 'prop-types';
 
-/** CustomLineChart component to display a line chart of data. Currently displays two lines: actual data and edited data */
-const CustomLineChart = ({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
-    <LineChart className="line-chart" data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="actual" stroke="#8884d8" activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="edited" stroke="#82ca9d" />
-    </LineChart>
-  </ResponsiveContainer>
-);
+/** CustomLineChart component to display a line chart of data. Automatically switches line labels based on the keys in the data */
+const CustomLineChart = ({ data }) => {
+  // Dynamically extract the keys from the first object in the data array
+  const dataKeys = Object.keys(data[0] || {}).filter(key => key !== 'name' && key !== 'amt');
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart className="line-chart" data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend formatter={(value) => value.charAt(0).toUpperCase() + value.slice(1)} />
+        {dataKeys.map((key, index) => (
+          <Line
+            key={key}
+            type="monotone"
+            dataKey={key}
+            stroke={index === 0 ? '#8884d8' : '#82ca9d'}
+            activeDot={{ r: 8 }}
+          />
+        ))}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
 // Require data to be passed to this component
 CustomLineChart.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
-      name: PropTypes.string,
+      name: PropTypes.string.isRequired,
       actual: PropTypes.number,
       edited: PropTypes.number,
       amt: PropTypes.number,

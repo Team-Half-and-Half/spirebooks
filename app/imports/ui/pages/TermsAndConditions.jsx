af823c8f42658b07d756fd1786c40b5c4bd8b9f9
Nicholas Beydler <beydlern@hawaii.edu>
2024-10-16 16:12:13 -1000

    i #61 TestCafe TermsAndConditions Implementation
    - TestCafe test cases were implemented to test the TermsAndConditions.jsx page.

--- app/imports/ui/pages/TermsAndConditions.jsx
+++ app/imports/ui/pages/TermsAndConditions.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const TermsAndConditions = () => (
   <Container id="termsandconditions-page" className="pt-3 text-center">
     <Row>
       <Col id="PP" className="justify-content-center">
-        <object data="/ToS-SpireBooks.pdf" type="application/pdf" width="75%" height="300%"> Hola </object>
+        <h1> Terms and Conditions </h1>
+        <object data="/ToS-SpireBooks.pdf" type="application/pdf" width="75%" height="300%"> <LoadingSpinner /> </object>
         {/* <p>ToS Link <a href="/ToS.pdf">to the PDF!</a></p> */}
       </Col>
     </Row>

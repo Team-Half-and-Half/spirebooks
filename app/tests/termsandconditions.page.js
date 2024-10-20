5ad161d7311efe88edbf315d60f5304e53579a11
Christopher Pascual <caspascual1293@gmail.com>
2024-10-20 10:43:32 -1000

    Update termsandconditions.page.js

--- app/tests/termsandconditions.page.js
+++ app/tests/termsandconditions.page.js
@@ -2,18 +2,18 @@ import { Selector, t } from 'testcafe';
 import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
 
 class TermsAndConditionsPage {
-    constructor() {
-        this.pageId = `#${PAGE_IDS.TOS}`;
-        this.pageSelector = Selector(this.pageId);
-    }
+  constructor() {
+    this.pageId = `#${PAGE_IDS.TOS}`;
+    this.pageSelector = Selector(this.pageId);
+  }
 
-    /* Asserts that this page is currently displayed. */
-    async isDisplayed() {
-        // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
-        // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
-        const termsAndConditionsContainer = Selector('#PP').exists;
-        await t.expect(termsAndConditionsContainer).ok({ timeout: 5000 });
-    }
+  /* Asserts that this page is currently displayed. */
+  async isDisplayed() {
+    // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
+    // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
+    const termsAndConditionsContainer = Selector('#PP').exists;
+    await t.expect(termsAndConditionsContainer).ok({ timeout: 5000 });
+  }
 }
 
 export const termsandconditionsPage = new TermsAndConditionsPage();

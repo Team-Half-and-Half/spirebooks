612e9f33d419596b26677c3ca6b978e725776feb
Samantha Mallari <samiam2204@gmail.com>
2024-10-21 17:00:15 -1000

    Added sliders mockup for applying volatility

--- app/imports/ui/pages/CompareProjections.jsx
+++ app/imports/ui/pages/CompareProjections.jsx
@@ -4,7 +4,8 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import ProjectionGraph from '../components/ProjectionGraph';
-import { multipleChartData } from '../utilities/TemporaryData'; // replace later when using real data
+import { multipleChartData } from '../utilities/TemporaryData';
+import ToggleSlider from '../components/ToggleSlider'; // replace later when using real data
 
 /** Compare projection graphs using dropdown menus */
 const CompareProjections = () => {
@@ -20,31 +21,55 @@ const CompareProjections = () => {
   const [dropdownOpen2, setDropdownOpen2] = useState(false); // Dropdown menu 2 is hidden initially
 
   return (
-    <Container fluid id={PAGE_IDS.COMPARE_PROJECTIONS}>
-      <Row>
-        <div>
-          <h1 className="company-title">{currentUser?.profile?.companyName || 'Company Name'}</h1>
-        </div>
-      </Row>
+    <Container fluid id={PAGE_IDS.COMPARE_PROJECTIONS} className="py-3">
       <Row>
         <Col>
-          {/* First Chart */}
+          <h1 className="company-title">{currentUser?.profile?.companyName || 'Company Name'}</h1>
+        </Col>
+      </Row>
+      <Row className="mx-auto justify-content-center pt-4">
+        <Col sm={12} md={5}>
           <ProjectionGraph
             selectedChart={selectedChart1}
             setSelectedChart={setSelectedChart1}
             dropdownOpen={dropdownOpen1}
             setDropdownOpen={setDropdownOpen1}
-            dropdownData={multipleChartData} // Assuming this is your full chart data array
+            dropdownData={multipleChartData}
           />
         </Col>
-        <Col>
-          {/* Second Chart */}
+        <Col sm={12} md={5}>
           <ProjectionGraph
             selectedChart={selectedChart2}
             setSelectedChart={setSelectedChart2}
             dropdownOpen={dropdownOpen2}
             setDropdownOpen={setDropdownOpen2}
-            dropdownData={multipleChartData} // Assuming this is your full chart data array
+            dropdownData={multipleChartData}
+          />
+        </Col>
+      </Row>
+      <Row className="py-3 mx-auto justify-content-center">
+        <Col sm={12} md={10}>
+          <h3>Apply Volatility Factors</h3>
+          <ToggleSlider
+            id="revenue-toggle"
+            uncheckedText="Apply Revenue %"
+            checkedText="Remove Revenue %"
+            max={20}
+            step={0.01}
+          />
+          <ToggleSlider
+            id="cost-toggle"
+            uncheckedText="Apply Cost %"
+            checkedText="Remove Cost %"
+            max={15}
+            step={0.01}
+          />
+          <ToggleSlider
+            id="interest-toggle"
+            uncheckedText="Apply Interest Rate %"
+            checkedText="Remove Interest Rate %"
+            max={5}
+            step={0.001}
           />
         </Col>
       </Row>

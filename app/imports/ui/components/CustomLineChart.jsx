59522b4f4c055cb6152ae2ceecb50b3283325689
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2024-11-05 20:02:41 -1000

    Cosmetic changes to graphs
    Made it so each y axis's domain is more distinct so the graphs are more readable. Also rounded the tooltip to 2 decimal places when hovering over points on graph.

--- app/imports/ui/components/CustomLineChart.jsx
+++ app/imports/ui/components/CustomLineChart.jsx
@@ -7,27 +7,33 @@ const CustomLineChart = ({ data }) => {
   // Dynamically extract the keys from the first object in the data array, excluding 'year'
   const dataKeys = Object.keys(data[0] || {}).filter(key => key !== 'year');
 
+  // Custom tooltip formatter to round to two decimal places and append 'M'
+  const tooltipFormatter = (value) => `${value.toFixed(2)}M`;
+
   return (
     <ResponsiveContainer className="px-3" width="100%" height={300}>
       <LineChart className="line-chart" data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="year" />
 
-        {/* Left Y-Axis with vertical label */}
+        {/* Left Y-Axis with dynamic scaling */}
         <YAxis
           yAxisId="left"
           stroke="#0073e6"
-          label={{ value: 'Millions', angle: -90, position: 'insideLeft', offset: 10 }} // Adjust offset as needed
+          domain={['auto', 'auto']}
+          label={{ value: 'Millions', angle: -90, position: 'insideLeft', offset: 10 }}
         />
-        {/* Right Y-Axis with vertical label */}
+
+        {/* Right Y-Axis with dynamic scaling */}
         <YAxis
           yAxisId="right"
           orientation="right"
           stroke="#8b0000"
-          label={{ value: 'Millions', angle: -90, position: 'insideRight', offset: 10 }} // Adjust offset as needed
+          domain={['auto', 'auto']}
+          label={{ value: 'Millions', angle: -90, position: 'insideRight', offset: 10 }}
         />
 
-        <Tooltip />
+        <Tooltip formatter={tooltipFormatter} />
         <Legend formatter={(value) => value.charAt(0).toUpperCase() + value.slice(1)} />
 
         {/* Assign the first dataKey to the left axis and the second to the right axis */}
@@ -37,7 +43,7 @@ const CustomLineChart = ({ data }) => {
             type="linear"
             dataKey={key}
             stroke={index === 0 ? '#0073e6' : '#8b0000'}
-            yAxisId={index === 0 ? 'left' : 'right'} // Use 'left' for first and 'right' for second key
+            yAxisId={index === 0 ? 'left' : 'right'}
             activeDot={{ r: 8 }}
           />
         ))}
@@ -54,7 +60,7 @@ CustomLineChart.propTypes = {
       actual: PropTypes.number,
       edited: PropTypes.number,
       amt: PropTypes.number,
-      year: PropTypes.number.isRequired, // Ensure year is included for proper type checking
+      year: PropTypes.number.isRequired,
     }),
   ).isRequired,
 };
